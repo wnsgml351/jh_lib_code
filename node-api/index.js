6ef9a0ec185a799a6dc69bc7bb5314d880f8ba23
wnsgml351 <wnsgml351@gmail.com>
2023-03-26 23:04:59 +0900

    테스트 코드 분리 / 서버 실행코드 분리

--- node-api/index.js
+++ node-api/index.js
@@ -4,16 +4,17 @@ var morgan = require("morgan");
 var bodyParser = require("body-parser");
 var user = require('./api/user');
 
-app.use(morgan("dev"));
+
+if (process.env.NODE_ENV  !== "test") {
+    app.use(morgan("dev"));
+}
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use('/users', user);
 
 
-
-app.listen(3000, function() {
-    console.log("server open 3000");
-});
 
 module.exports = app;
